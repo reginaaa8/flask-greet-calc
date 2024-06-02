63f6ccb6d5f6611af81586bc7d1e8df4800a5ffd
Regina Steffen <reginasteffen@live.com>
2024-06-02 17:46:31 -0500

    implemented '/add' route

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,13 @@
 # Put your app in here.
+from flask import Flask, request 
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route('/add')
+def run_add():
+    '''add a and b parameters'''
+    a = int(request.args.get('a'))
+    b = int(request.args.get('b'))
+    sum = add(a,b)
+    return f"{sum}"
